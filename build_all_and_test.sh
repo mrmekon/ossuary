dc6940442afb4ccd3d2ce6283cb85ac7ddb2779d
Trevor Bentley <trevor@trevorbentley.com>
2019-06-09 01:27:15 +0200

    support linux in build scripts

--- build_all_and_test.sh
+++ build_all_and_test.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 set -e
 
+case `uname` in
+    "Linux")
+	STRIP_ARGS=""
+	LIB_EXT="so"
+	XARGO_TARGET="x86_64-unknown-linux-gnu"
+	FIND_MODE="-maxdepth 1 -type f -perm /111"
+	;;
+    "Darwin")
+	STRIP_ARGS="-u -r -x -S"
+	LIB_EXT="dylib"
+	XARGO_TARGET="x86_64-apple-darwin"
+	FIND_MODE="-perm +111 -d 1 -type f"
+	;;
+    *)
+	echo "Platform not supported:" `uname`
+	exit 1
+	;;
+esac
+
 cargo test
 cargo bench -- --nocapture
+cargo build
 cargo build --examples
 cargo build --release --examples
-xargo build --target x86_64-apple-darwin --release
-strip -u -r -x -S target/release/libossuary.dylib
-strip -u -r -x -S target/x86_64-apple-darwin/release/libossuary.dylib
+xargo build --target $XARGO_TARGET --release
+strip $STRIP_ARGS "target/release/libossuary.$LIB_EXT"
+strip $STRIP_ARGS "target/$XARGO_TARGET/release/libossuary.$LIB_EXT"
 mkdir -p examples/build/
 pushd examples/build > /dev/null
 cmake ..
@@ -16,13 +36,13 @@ popd > /dev/null
 
 echo ""
 echo "Debug build:"
-ls target/debug/*.dylib
+ls target/debug/*.$LIB_EXT
 
 echo ""
 echo "Release build:"
-ls target/x86_64-apple-darwin/release/*.dylib
+ls target/$XARGO_TARGET/release/*.$LIB_EXT
 
 echo ""
 echo "Examples:"
-find target/debug/examples/ -type f -perm +111 -d 1
-find examples/build/ -type f -perm +111 -d 1
+find target/debug/examples/ $FIND_MODE
+find examples/build/ $FIND_MODE
